1afc544aa2da47c38fc6c4ce6257d4ea4c499ff1
Administrator <849875325@qq.com>
2020-05-23 17:22:33 +0800

    使用A_branch提交的版本

--- maven_service/src/main/java/com/it/service/ItemsService.java
+++ maven_service/src/main/java/com/it/service/ItemsService.java
@@ -6,4 +6,6 @@ public interface ItemsService {
     public Items findById(Integer id);
 
     public boolean addItems(Items item); //使用A_branch分支提交的版本
+
+    public void Delete(Integer id); //A分支天添加了一个删除方法
 }
