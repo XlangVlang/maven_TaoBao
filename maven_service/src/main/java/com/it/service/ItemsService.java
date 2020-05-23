5edd995b4ca84edd6851423e324474b80f98a929
Administrator <849875325@qq.com>
2020-05-23 17:17:25 +0800

    使用A_branch提交的版本

--- maven_service/src/main/java/com/it/service/ItemsService.java
+++ maven_service/src/main/java/com/it/service/ItemsService.java
@@ -6,4 +6,6 @@ public interface ItemsService {
     public Items findById(Integer id);
 
     public boolean addItems(Items item); //使用A_branch分支提交的版本
+
+    public boolean updateItems(Items item); //在主干又增加了一个update方法
 }
